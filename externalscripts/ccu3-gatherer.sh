449f59d76986e0827fc379f9e11fb00f2cc64103
Leo <57889616+ThisIsTenou@users.noreply.github.com>
2021-07-13 23:49:54 +0200

    Create ccu3-gatherer.sh

---
+++ externalscripts/ccu3-gatherer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+scriptloc=$(dirname "$(readlink -f "$0")")
+case "$2" in
+  discover-devices)
+    raw=$(curl -s -k "https://$1/addons/xmlapi/devicelist.cgi" | "$scriptloc/xml-to-json.py")
+    readarray -t name < <(echo "$raw" | jq .deviceList.device | jq '.[]["@name"]')
+    readarray -t iseid < <(echo "$raw" | jq .deviceList.device | jq '.[]["@ise_id"]')
+    readarray -t devicetype < <(echo "$raw" | jq .deviceList.device | jq '.[]["@device_type"]')
+    printf "["
+    for i in "${!iseid[@]}"; do
+      printf "{\"{#CCU3.ISEID}\":%s,\"{#CCU3.DEVICETYPE}\":%s,\"{#CCU3.NAME}\":%s}" "${iseid[i]}" "${devicetype[i]}" "${name[i]}"
+    if [[ "${iseid[i+1]}" ]]; then
+     printf ","
+    fi
+    done
+    printf "]\n"
+    ;;
+
+  *)
+    echo "Missing or invalid arguments!"
+    exit 1
+    ;;
+esac
